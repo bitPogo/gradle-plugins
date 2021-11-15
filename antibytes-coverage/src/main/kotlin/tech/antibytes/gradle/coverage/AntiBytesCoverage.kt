322ad208b0966c3f2c7a4848e2b5b7db75cc785e
Matthias Geisler <matthias.geisler@data4life.care>
2021-11-15 15:20:05 +0100

    Complete AntiBytesCoverage

--- antibytes-coverage/src/main/kotlin/tech/antibytes/gradle/coverage/AntiBytesCoverage.kt
+++ antibytes-coverage/src/main/kotlin/tech/antibytes/gradle/coverage/AntiBytesCoverage.kt
@@ -8,8 +8,23 @@ package tech.antibytes.gradle.coverage
 
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import tech.antibytes.gradle.coverage.task.TaskController
 
 class AntiBytesCoverage : Plugin<Project> {
     override fun apply(target: Project) {
+        if (target.plugins.findPlugin("jacoco") == null) {
+            target.plugins.apply("jacoco")
+        }
+
+        val extension = target.extensions.create(
+            "antiBytesCoverage",
+            AntiBytesCoverageExtension::class.java
+        )
+
+        target.evaluationDependsOnChildren()
+
+        target.afterEvaluate {
+            TaskController.configure(target, extension)
+        }
     }
 }
--- antibytes-coverage/src/test/kotlin/tech/antibytes/gradle/coverage/AntiBytesCoverageSpec.kt
+++ antibytes-coverage/src/test/kotlin/tech/antibytes/gradle/coverage/AntiBytesCoverageSpec.kt
@@ -6,8 +6,19 @@
 
 package tech.antibytes.gradle.coverage
 
+import io.mockk.Runs
+import io.mockk.every
+import io.mockk.just
+import io.mockk.mockk
+import io.mockk.mockkObject
+import io.mockk.unmockkObject
+import io.mockk.verify
 import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.plugins.PluginContainer
 import org.junit.Test
+import tech.antibytes.gradle.coverage.task.TaskController
+import tech.antibytes.gradle.publishing.invokeGradleAction
 import kotlin.test.assertTrue
 
 class AntiBytesCoverageSpec {
@@ -16,5 +27,77 @@ class AntiBytesCoverageSpec {
         val plugin: Any = AntiBytesCoverage()
 
         assertTrue(plugin is Plugin<*>)
+    }
+
+    @Test
+    fun `Given apply is called with a Project, it applies the JacocoPlugin, creates the Extension and processes it after the project was evaluated`() {
+        mockkObject(TaskController)
+        // Given
+        val project: Project = mockk()
+
+        val extension: AntiBytesCoverageExtension = mockk()
+        val plugins: PluginContainer = mockk()
+
+        every {
+            project.extensions.create("antiBytesCoverage", AntiBytesCoverageExtension::class.java)
+        } returns extension
+
+        every { project.plugins } returns plugins
+        every { plugins.findPlugin("jacoco") } returns null
+        every { plugins.apply(any()) } returns mockk()
+
+        every { project.evaluationDependsOnChildren() } just Runs
+
+        invokeGradleAction(
+            { probe -> project.afterEvaluate(probe) },
+            project
+        )
+
+        every { TaskController.configure(any(), any()) } just Runs
+
+        // When
+        AntiBytesCoverage().apply(project)
+
+        // Then
+        verify(exactly = 1) { plugins.apply("jacoco") }
+        verify(exactly = 1) { project.evaluationDependsOnChildren() }
+        verify(exactly = 1) { TaskController.configure(project, extension) }
+
+        unmockkObject(TaskController)
+    }
+
+    @Test
+    fun `Given apply is called with a Project, it will not applies the JacocoPlugin, if it is already present`() {
+        mockkObject(TaskController)
+        // Given
+        val project: Project = mockk()
+
+        val extension: AntiBytesCoverageExtension = mockk()
+        val plugins: PluginContainer = mockk()
+
+        every {
+            project.extensions.create("antiBytesCoverage", AntiBytesCoverageExtension::class.java)
+        } returns extension
+
+        every { project.plugins } returns plugins
+        every { plugins.findPlugin("jacoco") } returns mockk()
+        every { plugins.apply(any()) } returns mockk()
+
+        every { project.evaluationDependsOnChildren() } just Runs
+
+        invokeGradleAction(
+            { probe -> project.afterEvaluate(probe) },
+            project
+        )
+
+        every { TaskController.configure(any(), any()) } just Runs
+
+        // When
+        AntiBytesCoverage().apply(project)
+
+        // Then
+        verify(exactly = 0) { plugins.apply("jacoco") }
+
+        unmockkObject(TaskController)
     }
 }
