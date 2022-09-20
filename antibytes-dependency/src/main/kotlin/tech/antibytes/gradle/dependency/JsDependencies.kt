9aedebee38acb9b3f22d5fda2793313a38b7d3f2
Matthias Geisler <bitpogo@antibytes.tech>
2022-09-20 22:01:15 +0200

    Readd JsDependencies

---
+++ antibytes-dependency/src/main/kotlin/tech/antibytes/gradle/dependency/JsDependencies.kt
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2022 Matthias Geisler (bitPogo) / All rights reserved.
+ *
+ * Use of this source code is governed by Apache License, Version 2.0
+ */
+
+package tech.antibytes.gradle.dependency
+
+data class NpmPackage(
+    val name: String,
+    val version: String,
+)
+
+object JsDependencies {
+    val npm = Npm
+
+    object Npm {
+        val build = Build
+
+        object Build
+
+        val dev = Dev
+
+        object Dev
+
+        val peer = Peer
+
+        object Peer
+    }
+}
