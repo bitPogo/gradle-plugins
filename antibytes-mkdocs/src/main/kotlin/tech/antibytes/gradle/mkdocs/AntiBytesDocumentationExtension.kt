e032274bbdf4dcad183ac5328c73532034c57942
Matthias Geisler <bitpogo@antibytes.tech>
2022-12-30 01:36:32 +0100

    Remove Doublets in MkDocsConfig

--- antibytes-mkdocs/src/main/kotlin/tech/antibytes/gradle/mkdocs/AntiBytesDocumentationExtension.kt
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2022 Matthias Geisler (bitPogo) / All rights reserved.
- *
- * Use of this source code is governed by Apache License, Version 2.0
- */
-
-package tech.antibytes.gradle.mkdocs
-
-abstract class AntibytesDocumentationExtension : MkdocsContract.Extension
