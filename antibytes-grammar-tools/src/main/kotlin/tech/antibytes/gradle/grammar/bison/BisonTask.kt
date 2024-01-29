34fafc2dab98daf8ee9c07c906b084e20f615f92
Matthias Geisler <bitpogo@antibytes.tech>
2024-01-29 13:12:52 +0100

    Change BisonTask

--- antibytes-grammar-tools/src/main/kotlin/tech/antibytes/gradle/grammar/bison/BisonTask.kt
+++ antibytes-grammar-tools/src/main/kotlin/tech/antibytes/gradle/grammar/bison/BisonTask.kt
@@ -71,32 +71,41 @@ abstract class BisonTask : BisonTaskContract, DefaultTask() {
         }
     }
 
+    private fun MutableList<String>.addIf(
+        condition: Boolean,
+        value: () -> String,
+    ) {
+        if (condition) {
+            add(value())
+        }
+    }
+
     private fun addFileOptions(arguments: MutableList<String>) {
-        arguments.addIf(customSkeletonFile.isPresent, "--skeleton=${customSkeletonFile.get().asFile.absolutePath}")
-        arguments.addIf(header.isPresent, "--header=${header.get().asFile.absolutePath}")
-        arguments.addIf(reportFile.isPresent, "--report-file=${reportFile.get().asFile.absolutePath}")
-        arguments.addIf(graphFile.isPresent, "--graph=${graphFile.get().asFile.absolutePath}")
-        arguments.addIf(xmlReport.isPresent, "--xml=${xmlReport.get().asFile.absolutePath}")
-        arguments.addIf(style.isPresent, "--style=${style.get().asFile.absolutePath}")
+        arguments.addIf(customSkeletonFile.isPresent) { "--skeleton=${customSkeletonFile.get().asFile.absolutePath}" }
+        arguments.addIf(header.isPresent) { "--header=${header.get().asFile.absolutePath}" }
+        arguments.addIf(reportFile.isPresent) { "--report-file=${reportFile.get().asFile.absolutePath}" }
+        arguments.addIf(graphFile.isPresent) { "--graph=${graphFile.get().asFile.absolutePath}" }
+        arguments.addIf(xmlReport.isPresent) { "--xml=${xmlReport.get().asFile.absolutePath}" }
+        arguments.addIf(style.isPresent) { "--style=${style.get().asFile.absolutePath}" }
     }
 
     private fun addStringOptions(arguments: MutableList<String>) {
-        arguments.addIf(namePrefix.isPresent, "--name-prefix=${namePrefix.get()}")
-        arguments.addIf(filePrefix.isPresent, "--file-prefix=${filePrefix.get()}")
+        arguments.addIf(namePrefix.isPresent) { "--name-prefix=${namePrefix.get()}" }
+        arguments.addIf(filePrefix.isPresent) { "--file-prefix=${filePrefix.get()}" }
 
         if (report.get().isNotEmpty()) {
             val reportOptions = report.get().map { option -> option.value }
             arguments.add("--report=${reportOptions.joinToString(",")}")
         }
 
-        arguments.addIf(language.isPresent, "--language=${language.get()}")
+        arguments.addIf(language.isPresent) { "--language=${language.get()}" }
 
         if (features.get().isNotEmpty()) {
             val featureOptions = features.get().map { option -> option.value }
             arguments.add("--feature=${featureOptions.joinToString(",")}")
         }
 
-        arguments.addIf(color.isPresent, "--color=${color.get().value}")
+        arguments.addIf(color.isPresent) { "--color=${color.get().value}" }
     }
 
     private fun concatenateMapOption(map: Map<String, String>): String {
@@ -106,30 +115,24 @@ abstract class BisonTask : BisonTaskContract, DefaultTask() {
     }
 
     private fun addMapOptions(arguments: MutableList<String>) {
-        arguments.addIf(define.get().isNotEmpty(), "--define=${concatenateMapOption(define.get())}")
-        arguments.addIf(forceDefine.get().isNotEmpty(), "--force-define=${concatenateMapOption(forceDefine.get())}")
-        arguments.addIf(filePrefixMap.get().isNotEmpty(), "--file-prefix-map=${concatenateMapOption(filePrefixMap.get())}")
+        arguments.addIf(define.get().isNotEmpty()) { "--define=${concatenateMapOption(define.get())}" }
+        arguments.addIf(forceDefine.get().isNotEmpty()) { "--force-define=${concatenateMapOption(forceDefine.get())}" }
+        arguments.addIf(filePrefixMap.get().isNotEmpty()) {"--file-prefix-map=${concatenateMapOption(filePrefixMap.get())}" }
     }
 
     private fun addErrorOptions(arguments: MutableList<String>) {
         var warnings = if (warnings.get().isNotEmpty()) {
-            warnings.get()
-                .map { option -> option.value }
-                .joinToString(",")
+            warnings.get().joinToString(",") { option -> option.value }
         } else {
             ""
         }
 
         if (errors.get().isNotEmpty()) {
-            val errors = errors.get()
-                .map { option -> "error=${option.value}" }
-                .joinToString(",")
+            val errors = errors.get().joinToString(",") { option -> "error=${option.value}" }
             warnings += ",$errors"
         }
 
-        if (warnings.isNotEmpty()) {
-            arguments.add("--warning=${warnings.trimStart(',')}")
-        }
+        arguments.addIf(warnings.isNotEmpty()) { "--warning=${warnings.trimStart(',')}" }
     }
 
     private fun assembleOptions(): MutableList<String> {
